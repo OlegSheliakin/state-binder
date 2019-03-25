ea8fa2ca26fff636b8cdceafd65fc9634e92cacd
Oleg Shelyakin <oshelyakin@smedialink.com>
2019-03-25 13:51:18 +0300

    refactored MainState

--- app/src/main/java/com/olegsheliakin/statebinder/MainState.kt
+++ app/src/main/java/com/olegsheliakin/statebinder/MainState.kt
@@ -1,10 +1,6 @@
 package com.olegsheliakin.statebinder
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class MainState(
     val label: String,
     val errorText: String?
-) : Parcelable, State
+) : State
